937c8903846021ccf368c24ee5ed16ea71305493
Shaden Alzaid <shadenalzaid@hotmail.com>
2021-10-19 14:50:44 +0300

    enum print added

--- Vehicle/Car/Car.cs
+++ Vehicle/Car/Car.cs
@@ -20,6 +20,7 @@ namespace Dealership
             {  
                 Console.WriteLine($"{i}. {Enum.GetName(typeof(VehicleColors), i)}");  
             }
+
             Console.Write("Choose Color number: ");
             var choice1 = int.Parse(Console.ReadLine());
             this.Color = (VehicleColors) choice1;
@@ -28,6 +29,7 @@ namespace Dealership
             {  
                 Console.WriteLine($"{i}. {Enum.GetName(typeof(CarModels), i)}");  
             }
+            
             Console.Write("Choose Model Number: ");
             var choice2 = int.Parse(Console.ReadLine());
             this.CModel = (CarModels) choice2;
