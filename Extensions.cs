d0e6f36726b80beb304927c8df5c6ead15925ca9
Shaden Alzaid <shadenalzaid@hotmail.com>
2021-10-21 14:29:57 +0300

    GetMenu added

--- Extensions.cs
+++ Extensions.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dealership.Vehicle;
 
 namespace Dealership
 {
@@ -17,6 +18,7 @@ namespace Dealership
             }
         }
  
+
         public static int GetSelectedOptions<T>()where T : System.Enum
         {
             
@@ -27,22 +29,63 @@ namespace Dealership
             foreach (int item in values)
                 options.Add(item);
 
+            Console.Write("Enter you choice number: ");            
             var input = Console.ReadLine();
-            bool v = int.TryParse(input, out int x);
 
             // check if parsed input is in enum 
-            if (v == true){ //if true return parsed value
-                foreach(int z in Enumerable.Range(1,Enum.GetNames(typeof(T)).Length)){
-                    if(int.Parse(input) == z){
-                        return int.Parse(input);
+            if (int.TryParse(input, out int x)){ //if true return parsed value
+                foreach(int z in Enumerable.Range(1, Enum.GetNames(typeof(T)).Length))
+                {
+                    if(x == z)
+                    {
+                        return x;
                     }  
                 }
-            }else
-                Console.Write("Please re-enter your choice! "); 
-                return GetSelectedOptions<T>();
+            }
+            Console.Write("Please re-enter your choice! "); 
+            return GetSelectedOptions<T>();
+        }
+
+
+        public static void GetMenu(){
+          var shoppingCart = new List<IVehicle>();
+            IVehicle vehicle = null;
+
+            Console.WriteLine("\n");
+            Console.WriteLine("**********************************");
+            Console.WriteLine("WELCOME TO OUR VEHICLE DEALERSHIP!\n\n");
+            Console.WriteLine("WHAT ARE YOU INTERESTED IN?");
+            
+            Extensions.PrintEnum(typeof(VehicleTypes));
+
+            Console.WriteLine("**********************************");
+            Console.WriteLine("\n");
+            Console.Write("Enter you choice number: ");
+            
+            var choice = (int)(Extensions.GetSelectedOptions<VehicleTypes>());
+            var type = (VehicleTypes)choice;
+
+            //Apply SOLID   learn(SO) in solid
+            switch (type)
+            {
+                case VehicleTypes.Car:
+                    vehicle = new Car();
+                    break;
+                case VehicleTypes.Motorcycle:
+                    vehicle = new Motorcycle();
+                    break;
+                case VehicleTypes.Truck:
+                    vehicle = new Truck();
+                    break;
+                default:
+                    break;
+            }
+
+            vehicle.GetOptions();
+            Console.Write(vehicle.ToString());
+
+            shoppingCart.Add(vehicle);
 
         }
-        
-
     }
 }
--- Vehicle/VehicleFactory.cs
+++ Vehicle/VehicleFactory.cs
@@ -9,45 +9,7 @@ namespace Dealership.Vehicle
 
         public void VehicleFactoryOrder()
         {
-
-            var shoppingCart = new List<IVehicle>();
-            IVehicle vehicle = null;
-
-            Console.WriteLine("\n");
-            Console.WriteLine("**********************************");
-            Console.WriteLine("WELCOME TO OUR VEHICLE DEALERSHIP!\n\n");
-            Console.WriteLine("WHAT ARE YOU INTERESTED IN?");
-            
-            Extensions.PrintEnum(typeof(VehicleTypes));
-
-            Console.WriteLine("**********************************");
-            Console.WriteLine("\n");
-            Console.Write("Enter you choice number: ");
-            
-            var choice = (int)(Extensions.GetSelectedOptions<VehicleTypes>());
-            var type = (VehicleTypes)choice;
-
-            //Apply SOLID   learn(SO) in solid
-            switch (type)
-            {
-                case VehicleTypes.Car:
-                    vehicle = new Car();
-                    break;
-                case VehicleTypes.Motorcycle:
-                    vehicle = new Motorcycle();
-                    break;
-                case VehicleTypes.Truck:
-                    vehicle = new Truck();
-                    break;
-                default:
-                    break;
-            }
-
-            vehicle.GetOptions();
-            Console.Write(vehicle.ToString());
-
-            shoppingCart.Add(vehicle);
-
+                Extensions.GetMenu();
         }
 
     }
