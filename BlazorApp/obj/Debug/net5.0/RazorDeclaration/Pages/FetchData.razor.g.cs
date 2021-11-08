62c479b5f590914ed89af4451f1dc6f94801e656
Shaden Alzaid <shadenalzaid@hotmail.com>
2021-11-08 16:09:30 +0300

    idk

--- BlazorApp/obj/Debug/net5.0/RazorDeclaration/Pages/FetchData.razor.g.cs
+++ BlazorApp/obj/Debug/net5.0/RazorDeclaration/Pages/FetchData.razor.g.cs
@@ -13,78 +13,71 @@ namespace BlazorApp.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\ysallam\source\repos\Dealership\BlazorApp\_Imports.razor"
+#line 1 "C:\Users\salzaid\dealership\Dealership\BlazorApp\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\ysallam\source\repos\Dealership\BlazorApp\_Imports.razor"
+#line 2 "C:\Users\salzaid\dealership\Dealership\BlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\ysallam\source\repos\Dealership\BlazorApp\_Imports.razor"
+#line 3 "C:\Users\salzaid\dealership\Dealership\BlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\ysallam\source\repos\Dealership\BlazorApp\_Imports.razor"
+#line 4 "C:\Users\salzaid\dealership\Dealership\BlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\ysallam\source\repos\Dealership\BlazorApp\_Imports.razor"
+#line 5 "C:\Users\salzaid\dealership\Dealership\BlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\ysallam\source\repos\Dealership\BlazorApp\_Imports.razor"
+#line 6 "C:\Users\salzaid\dealership\Dealership\BlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\ysallam\source\repos\Dealership\BlazorApp\_Imports.razor"
+#line 7 "C:\Users\salzaid\dealership\Dealership\BlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\ysallam\source\repos\Dealership\BlazorApp\_Imports.razor"
+#line 8 "C:\Users\salzaid\dealership\Dealership\BlazorApp\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\ysallam\source\repos\Dealership\BlazorApp\_Imports.razor"
+#line 9 "C:\Users\salzaid\dealership\Dealership\BlazorApp\_Imports.razor"
 using BlazorApp;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\ysallam\source\repos\Dealership\BlazorApp\_Imports.razor"
+#line 10 "C:\Users\salzaid\dealership\Dealership\BlazorApp\_Imports.razor"
 using BlazorApp.Shared;
-
-#line default
-#line hidden
-#nullable disable
-#nullable restore
-#line 3 "C:\Users\ysallam\source\repos\Dealership\BlazorApp\Pages\FetchData.razor"
-using BlazorApp.Data;
 
 #line default
 #line hidden
@@ -97,20 +90,6 @@ using BlazorApp.Data;
         {
         }
         #pragma warning restore 1998
-#nullable restore
-#line 39 "C:\Users\ysallam\source\repos\Dealership\BlazorApp\Pages\FetchData.razor"
-       
-    private WeatherForecast[] forecasts;
-
-    protected override async Task OnInitializedAsync()
-    {
-        forecasts = await ForecastService.GetForecastAsync(DateTime.Now);
-    }
-
-#line default
-#line hidden
-#nullable disable
-        [global::Microsoft.AspNetCore.Components.InjectAttribute] private WeatherForecastService ForecastService { get; set; }
     }
 }
 #pragma warning restore 1591
