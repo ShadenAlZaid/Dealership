3190b66969a48201a2afdb12311ad118b9f23257
ShadenAlZaid <62605834+ShadenAlZaid@users.noreply.github.com>
2021-11-17 15:08:36 +0300

    index

--- DealerShip.Console/Program.cs
+++ DealerShip.Console/Program.cs
@@ -1,32 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-using DealershipLibrary.Vehicle.Cars;
-using DealershipLibrary;
-using DealershipLibrary.Vehicle;
+﻿// // See https://aka.ms/new-console-template for more information
+// using DealershipLibrary.Vehicle.Cars;
+// using DealershipLibrary;
+// using DealershipLibrary.Vehicle;
 
-// var shoppingCart = new List<IVehicle>();
+// // var shoppingCart = new List<IVehicle>();
 
-    IVehicle vehicle = null;
-    IVehicleFactory vehicleFactory = null;
+//     IVehicle vehicle = null;
+//     IVehicleFactory vehicleFactory = null;
 
-    Extensions.PrintEnum(typeof(VehicleTypes));
+//     Extensions.PrintEnum(typeof(VehicleTypes));
 
-    var choice = (int)(Extensions.GetSelectedOptions<VehicleTypes>());
-    var type = (VehicleTypes)choice;
+//     var choice = (int)(Extensions.GetSelectedOptions<VehicleTypes>());
+//     var type = (VehicleTypes)choice;
 
-    //Apply SOLID   learn(SO) in solid
-    switch (type)
-    {
-        case VehicleTypes.Car:
-            vehicleFactory = new CarFactory();
-            break;
-        case VehicleTypes.Motorcycle:
-            vehicleFactory = new MCFactory();
-        break;
-        case VehicleTypes.Truck:
-            vehicleFactory = new TruckFactory();
-        break;
-        default:
-            break;
-    }
+//     //Apply SOLID   learn(SO) in solid
+//     switch (type)
+//     {
+//         case VehicleTypes.Car:
+//             vehicleFactory = new CarFactory();
+//             break;
+//         case VehicleTypes.Motorcycle:
+//             vehicleFactory = new MCFactory();
+//         break;
+//         case VehicleTypes.Truck:
+//             vehicleFactory = new TruckFactory();
+//         break;
+//         default:
+//             break;
+//     }
 
-    vehicle = vehicleFactory.CreateVehicle();
+//     vehicle = vehicleFactory.CreateVehicle();
