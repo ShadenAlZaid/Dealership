441527c12ff882876af5a38af40cf20c2ecfe158
Shaden Abdullah Alzaid <62605834+ShadenAlZaid@users.noreply.github.com>
2022-01-06 13:15:41 +0300

    dataseeding
    IdentityUser
    IdentityRole
    IdentityUserRole

--- Dealership.BlazorApp/UserDbContext.cs
+++ Dealership.BlazorApp/UserDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -11,15 +12,44 @@ namespace Dealership.BlazorApp.Data
             : base(options)
         {
         }
+        public DbSet<IdentityUser> Users { get; set; }
+        public DbSet<IdentityRole> Roles { get; set; }
+        public DbSet<Microsoft.AspNet.Identity.EntityFramework.IdentityUserRole> UserRoles { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             ConfigureUsers(modelBuilder);
+            ConfigureRoles(modelBuilder);
+            ConfigureUserRoles(modelBuilder);
+
+        }
+        private static void ConfigureUsers(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<IdentityUser>()
+                .HasData(
+                    new List<IdentityUser<Guid>>() {
+                        new IdentityUser<Guid>("aff3b9d0-2e80-4582-923b-d238e5383b70")
+                    });
         }
 
-        private void ConfigureUsers(ModelBuilder modelBuilder)
+        private static void ConfigureRoles(ModelBuilder modelBuilder)
         {
-            throw new NotImplementedException();
+            modelBuilder.Entity<IdentityRole>()
+                .HasData(
+                    new List<IdentityRole>() {
+                        new IdentityRole("Admin")
+                    });
+        }
+
+        private static void ConfigureUserRoles(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<IdentityUserRole<Guid>>()
+                .HasData(
+                    new List<IdentityUserRole<Guid>>() {
+                        new IdentityUserRole<Guid>()
+                    }) ;
         }
     }
 }
+
+//"aff3b9d0-2e80-4582-923b-d238e5383b70",
